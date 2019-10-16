539dcce3750268ede2f7376f4617db43eaf131bb
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-10-16 11:58:16 +0200

    Move QuickGO to new component

---
+++ src/components/analysis/functional/QuickGOCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card>
+      <v-card-title>QuickGo biological process</v-card-title>
+      <v-card-text>
+
+      </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import {Prop, Watch} from "vue-property-decorator";
+import GoTerm from '@/logic/functional-annotations/GoTerm';
+
+@Component
+export default class QuickGOCard extends Vue {
+    @Prop({required: true})
+    private goTerms: GoTerm[];
+
+    
+}
+</script>
+
+<style>
+
+</style>
