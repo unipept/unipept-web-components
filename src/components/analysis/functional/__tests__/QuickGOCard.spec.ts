2cea4ab453623d8560ff046b7e81d111b68c76bd
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-10-21 08:40:33 +0200

    Unit tests are working now

--- src/components/analysis/functional/__tests__/QuickGOCard.spec.ts
+++ src/components/analysis/functional/__tests__/QuickGOCard.spec.ts
@@ -1,10 +1,25 @@
 import jest from "jest";
-import { shallowMount } from '@vue/test-utils'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import QuickGOCard from "./../QuickGOCard.vue";
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify);
+
+const localVue = createLocalVue();
 
 describe('QuickGOSummaryCard', () => {
+    let vuetify;
+
+    beforeEach(() => {
+      vuetify = new Vuetify()
+    })
+
     it('renders a placeholder when no GO-terms are found', () => {
+
         const wrapper = shallowMount(QuickGOCard, {
+            localVue,
+            vuetify,
             propsData: {
                 items: [],
                 sortSettings: null
