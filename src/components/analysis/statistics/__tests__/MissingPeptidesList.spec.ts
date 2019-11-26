d641047741922c36e6201e4902f01feb72dccfbc
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-11-26 15:11:01 +0100

    Working on tests for experiment summary

---
+++ src/components/analysis/statistics/__tests__/MissingPeptidesList.spec.ts
@@ -0,0 +1,19 @@
+// import { shallowMount, createLocalVue } from "@vue/test-utils"
+// import MissingPeptidesList from "./../MissingPeptidesList.vue";
+// import Vue from "vue"
+// import Vuetify from "vuetify"
+
+// Vue.use(Vuetify);
+
+// const localVue = createLocalVue();
+
+// describe("MissingPeptidesList", () => {
+//     let vuetify;
+//     let el;
+
+//     beforeEach(() => {
+//         vuetify = new Vuetify();
+//     });
+
+//     it("correctly renders all selected items")
+// })
