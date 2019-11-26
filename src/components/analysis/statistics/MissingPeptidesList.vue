a145ae4d1c0d54492f66a48c9890ca1b2810b4d9
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-11-26 13:27:35 +0100

    Redesign missing peptides list

--- src/components/analysis/statistics/MissingPeptidesList.vue
+++ src/components/analysis/statistics/MissingPeptidesList.vue
@@ -1,33 +1,26 @@
 <template>
-    <span v-if="!dataset || loading" style="position: relative; left: 50%; transform: translateX(-50%);">
-        <v-progress-circular :size="50" :width="5" color="primary"></v-progress-circular>
-    </span>
-    <div v-else>
-        <v-row>
-            <v-col :cols="7">
-                <span>
-                    Sorry, we didn't manage to find {{ missedPeptides.length }} of your 
-                    peptides. You can BLAST them by clicking the links or copy them by using the button on 
-                    the right.
-                </span>
-            </v-col>
-            <v-col :cols="5">
-                <v-btn @click="copyToClipboard">
-                    <v-icon left>
-                        mdi-clipboard-text-outline
-                    </v-icon>
-                    Copy to clipboard
-                </v-btn>
-            </v-col>
-        </v-row>
+    <div v-if="dataset && !loading" style="margin-top: 16px;">
+        <div>
+            Sorry, we didn't manage to find {{ missedPeptides.length }} of your 
+            peptides. You can BLAST them by clicking the links or copy them by using the button below.
+        </div>
         
-        <ul>
-            <li v-for="missed of missedPeptides" :key="missed">
-                <a :href="getBlastUrl(missed)" target="_blank">
-                    {{ missed }}
-                </a>
-            </li>
-        </ul>
+        <v-data-table :headers="headers" :items="missedPeptides" :items-per-page="10">
+            <template v-slot:item.action="{ item }">
+                <tooltip message="BLAST this peptide in an external window.">
+                    <v-icon @click="openBlastUrl(item.value)">mdi-open-in-new</v-icon>
+                </tooltip>
+            </template>
+        </v-data-table>
+
+        <div style="margin-top: 8px; margin-bottom: 8px; display: flex; justify-content: center;">
+            <v-btn @click="copyToClipboard" color="primary">
+                <v-icon left>
+                    mdi-clipboard-text-outline
+                </v-icon>
+                Copy all to clipboard
+            </v-btn>
+        </div>
     </div>
 </template>
 
@@ -37,15 +30,38 @@ import { Component, Prop, Watch } from "vue-property-decorator";
 import Assay from "../../../logic/data-management/assay/Assay";
 import TaxaDataSource from "../../../logic/data-source/TaxaDataSource";
 import * as clipboard from "clipboard-polyfill";
+import Utils from "./../../custom/Utils";
+import Tooltip from "./../../custom/Tooltip.vue";
 
-@Component
+@Component({
+    components: {
+        Tooltip
+    }
+})
 export default class MissingPeptidesList extends Vue {
     @Prop({ required: true })
     private dataset: Assay;
 
-    private missedPeptides: string[] = [];
+    private missedPeptides: {value: string}[] = [];
 
     private loading: boolean = true;
+
+    private headers = [
+        {
+            text: "Peptide",
+            align: "left",
+            sortable: true,
+            width: "80%",
+            value: "value"
+        }, 
+        {
+            text: "Actions",
+            align: "center",
+            sortable: false,
+            width: "20%",
+            value: "action"
+        }
+    ]
     
     mounted() {
         this.missedPeptides = []
@@ -57,19 +73,24 @@ export default class MissingPeptidesList extends Vue {
         if (this.dataset) {
             this.loading = true;
             let taxaSource: TaxaDataSource = await this.dataset.dataRepository.createTaxaDataSource();
-            this.missedPeptides = await taxaSource.getMissedPeptides();
+            this.missedPeptides = (await taxaSource.getMissedPeptides()).map((item: string) => {
+                return {
+                    value: item
+                }
+            });
             this.loading = false;
         }
     }
 
-    private getBlastUrl(peptide: string) {
-        return "http://blast.ncbi.nlm.nih.gov/Blast.cgi?PAGE_TYPE=BlastSearch&SET_SAVED_SEARCH=on" +
+    private openBlastUrl(peptide: string) {
+        const url: string = "http://blast.ncbi.nlm.nih.gov/Blast.cgi?PAGE_TYPE=BlastSearch&SET_SAVED_SEARCH=on" +
             "&USER_FORMAT_DEFAULTS=on&PAGE=Proteins&PROGRAM=blastp&QUERY=" + peptide + "&GAPCOSTS=11%201" + 
             "&EQ_MENU=Enter%20organism%20name%20or%20id--completions%20will%20be%20suggested&DATABASE=nr" +
             "&BLAST_PROGRAMS=blastp&MAX_NUM_SEQ=100&SHORT_QUERY_ADJUST=on&EXPECT=10&WORD_SIZE=3" + 
             "&MATRIX_NAME=BLOSUM62&COMPOSITION_BASED_STATISTICS=2&SHOW_OVERVIEW=on&SHOW_LINKOUT=on" + 
             "&ALIGNMENT_VIEW=Pairwise&MASK_CHAR=2&MASK_COLOR=1&GET_SEQUENCE=on&NEW_VIEW=on&NUM_OVERVIEW=100" + 
             "&DESCRIPTIONS=100&ALIGNMENTS=100&FORMAT_OBJECT=Alignment&FORMAT_TYPE=HTML&OLD_BLAST=false"
+        Utils.openInBrowser(url);
     }
 
     private copyToClipboard() {
