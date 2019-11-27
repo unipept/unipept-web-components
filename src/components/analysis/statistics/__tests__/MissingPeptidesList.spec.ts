394b65626631e5e00edb885286bb51d15bf4684c
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-11-27 16:45:07 +0100

    Fixed all tests for MissedPeptidesList

--- src/components/analysis/statistics/__tests__/MissingPeptidesList.spec.ts
+++ src/components/analysis/statistics/__tests__/MissingPeptidesList.spec.ts
@@ -5,6 +5,7 @@ import Vuetify from "vuetify";
 import Vuex from "vuex";
 import Mock from "@/test/Mock";
 import Assay from "@/logic/data-management/assay/Assay";
+import flushPromises from "flush-promises"
 
 Vue.use(Vuetify);
 Vue.use(Vuex);
@@ -35,9 +36,9 @@ describe("MissingPeptidesList", () => {
         });
     });
 
-    it("correctly renders all peptides ", (done) => {
+    it("correctly renders all peptides", (done) => {
         let mock: Mock = new Mock();
-        mock.mockInitializedAssay().then((assay: Assay) => {
+        mock.mockInitializedAssay().then(async(assay: Assay) => {
             const wrapper = mount(MissingPeptidesList, {
                 store,
                 localVue,
@@ -47,13 +48,42 @@ describe("MissingPeptidesList", () => {
                 }
             });
     
-            // Wait for all async operations to be finished, before expecting anything.
-            const flushPromises = () => new Promise(setImmediate);
-            flushPromises().then(() => {
-                expect(wrapper.html()).toMatchSnapshot();
-                // Call done here in the promise resolve.
-                done();
-            })
+            await sleep(1000);
+            await flushPromises();
+
+            // Test whether the calculated amount of missing peptides is correct.
+            expect(wrapper.find("div > div").html()).toContain("we didn't manage to find 10 of your");
+            expect(wrapper.find("div > div").html()).toMatchSnapshot();
+
+            // Test if all peptides that should be found are rendered.
+            const foundPeptides: string[] = [
+                "SGLAPFYSDK",
+                "AAANESFGYNEDELVSSDLVGMR",
+                "LAEEVLR",
+                "YEVGTMLELPR",
+                "TNTLLQSAFFK",
+                "ANFEGECSEVGMYLAMAR",
+                "MEVAVGDK",
+                "LLDLGVLVGSGYHVNPK",
+                "VYFLNFKPESSDEWK",
+                "LGLVAVSR"
+            ]
+
+            const dataTable = wrapper.find(".v-data-table").html();
+
+            for (let peptide of foundPeptides) {
+                expect(dataTable).toContain(peptide);
+            }
+
+            // Test if no more peptides are rendered.
+            expect(wrapper.findAll("td.text-left").length).toEqual(10);
+
+            done();
         });
     })
 })
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
