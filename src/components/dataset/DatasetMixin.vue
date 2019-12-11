4ca34aa452a5ebb6ce80bbe13546eb2f929f241f
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-12-11 18:57:12 +0100

    Minor changes

--- src/components/dataset/DatasetMixin.vue
+++ src/components/dataset/DatasetMixin.vue
@@ -8,16 +8,28 @@ import { EventBus } from "../EventBus";
 import Assay from "../../logic/data-management/assay/Assay";
 
 @Component
+/**
+ * The CreateDatasetCard provides the user with a form for creating a new Assay, based on a list of peptides. The user 
+ * can indicate it's preference to storing this card in persistent storage. Note that this component does not stores or 
+ * selects assays by itself, it only emits an event with it's intended action.
+ */
 export default class DatasetMixin extends Vue {
     protected pendingStore: boolean = false;
 
-    protected selectDataset(dataset: Assay): void {
-        EventBus.$emit("select-dataset", dataset);
+    protected selectDataset(assay: Assay): void {
+        /**
+         * Fired after creation of a new assay by the user. This assay is not automatically processed or added to the
+         * list of selected assays by this component.
+         * 
+         * @event create-dataset
+         * @property {Assay} assay The new assay that was created by the user.
+         */
+        this.$emit("create-dataset", assay);
     }
 
     protected deleteDataset(dataset: Assay): void {
         this.$store.dispatch("deleteDataset", dataset);
-        EventBus.$emit("delete-dataset", dataset);
+        this.$emit("delete-dataset", dataset);
     }
 
     protected storeDataset(peptides: string, name: string, save: boolean): void {
