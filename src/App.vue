020e8ef357cf9ab9321ee4f34b3ad4eab44a609b
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-12-11 16:26:04 +0100

    Basic documentation generation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <v-app>
-    <analysis-component :selected-datasets="[]" :stored-datasets="[]"></analysis-component>
+    <p>This is a test...</p>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Component from "vue-class-component";
-import SelectDatasetsCard from "./components/dataset/SelectDatasetsCard.vue";
-import AnalysisComponent from "./components/dataset/AnalysisComponent.vue";
 
 @Component({
-    components: { SelectDatasetsCard, AnalysisComponent }
+    components: {  }
 })
 export default class App extends Vue {
   private selectedDatasets = [];
