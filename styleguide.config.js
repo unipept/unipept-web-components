30f62c4a41b481139cdfd185e0341c893e0d8cd9
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2019-12-11 16:44:57 +0100

    Improve styleguide config

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,16 +1,32 @@
 module.exports = {
 	// set your styleguidist configuration here
-	title: 'Default Style Guide',
-	// components: 'src/components/**/[A-Z]*.vue',
-	// defaultExample: true,
-	// sections: [
-	//   {
-	//     name: 'First Section',
-	//     components: 'src/components/**/[A-Z]*.vue'
-	//   }
-	// ],
-	// webpackConfig: {
-	//   // custom config goes here
-	// },
+    title: 'Unipept Web Components',
+    components: 'src/components/**/[A-Z]*.vue',
+    sections: [
+        {
+            name: 'analysis',
+            components: 'src/components/analysis/**/[A-Z]*.vue'
+        }, 
+        {
+            name: 'custom',
+            components: 'src/components/custom/**/[A-Z]*.vue'
+        },
+        {
+            name: 'dataset',
+            components: 'src/components/dataset/**/[A-Z]*.vue'
+        },
+        {
+            name: 'heatmap',
+            components: 'src/components/heatmap/**/[A-Z]*.vue'
+        },
+        {
+            name: 'tables',
+            components: 'src/components/tables/**/[A-Z]*.vue'
+        },
+        {
+            name: 'visualizations',
+            components: 'src/components/visualizations/**/[A-Z]*.vue'
+        }
+    ],
 	exampleMode: 'expand'
 }
