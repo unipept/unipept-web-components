28113ef73544d6fc4135e43eacd2577109e6c97b
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-03-28 10:12:14 +0100

    Add ability to specify download-format

--- src/logic/data-source/ExportManager.ts
+++ src/logic/data-source/ExportManager.ts
@@ -9,35 +9,37 @@ import { PeptideData } from "../api/pept2data/Response";
 
 export default class ExportManager {
     // TODO this code should be cleaned up, once all the proteincounts are part of the data sources.
-    public async exportResultsAsCsv(repo: MetaProteomicsDataRepository): Promise<string> {
+    public async exportResultsAsCsv(
+        repo: MetaProteomicsDataRepository,
+        separator: string = ",",
+        goSeparator: string = ";"
+    ): Promise<string> {
         const taxaSource = await repo.createTaxaDataSource();
         const taxos: TaxonomicsSummary[] = await taxaSource.getTaxonomicSummaries();
 
-        const ecSource = await repo.createEcDataSource();
         const goSource = await repo.createGoDataSource();
 
         // These need to be manually specified here to retain a specific order!
         const goNameSpaces: GoNameSpace[] = [
             GoNameSpace.BiologicalProcess,
-            GoNameSpace.CellularComponent, 
+            GoNameSpace.CellularComponent,
             GoNameSpace.MolecularFunction
         ];
 
-        let result: string = 
-            "peptide,lca," + Object.values(TaxumRank).join(",") + "," + "EC," + 
-            goNameSpaces.map(ns => `GO (${ns})`).join(",") + "\n";
+        let result: string =
+            `peptide${separator}lca${separator}${Object.values(TaxumRank).join(separator)}${separator}EC${separator}${goNameSpaces.map(ns => `GO (${ns})`).join(separator)}\n`;
 
         const processedContainer: ProcessedPeptideContainer = await repo.initProcessedPeptideContainer();
 
         for (const tax of taxos) {
             // Process taxonomic information
-            let row = tax.sequence + "," + tax.lcaName + "," + tax.lineageNames.map(e => {
+            let row = tax.sequence + separator + tax.lcaName + separator + tax.lineageNames.map(e => {
                 if (e === null) {
                     return "";
                 } else {
                     return e;
                 }
-            }).join(",");
+            }).join(separator);
 
 
             const peptData: PeptideData = processedContainer.response.get(tax.sequence);
@@ -52,20 +54,20 @@ export default class ExportManager {
 
             const ecNumbers = dataList.filter(x => x.code.startsWith("EC:")).sort((a, b) => b.count - a.count);
 
-            row += ",";
+            row += separator;
             row += ecNumbers
                 .slice(0, 3)
                 .map(a => `${a.code.substr(3)} (${this.numberToPercent(a.count / peptData.fa.counts.EC)})`)
-                .join(";");
-            row += ",";
+                .join(goSeparator);
+            row += separator;
 
             row += (await Promise.all(goNameSpaces.map(async ns => {
                 const goTerms = (await goSource.getGoTerms(ns, 0, [tax.sequence])).map(x => {
                     return dataList.find(y => y.code === x.code);
                 }).sort((a, b) => b.count - a.count);
 
-                return goTerms.slice(0, 3).map(a => `${a.code} (${this.numberToPercent(a.count / peptData.fa.counts.GO)})`).join(";");
-            }))).join(",");
+                return goTerms.slice(0, 3).map(a => `${a.code} (${this.numberToPercent(a.count / peptData.fa.counts.GO)})`).join(goSeparator);
+            }))).join(separator);
 
             row += "\n";
             // Duplicate row in the case the peptide was more present more than once.
