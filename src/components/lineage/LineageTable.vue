620cb1be22e0e4773d56e597426cb12e0e65f2be
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-04-09 08:41:11 +0200

    New files for implementing LineageTable

---
+++ src/components/lineage/LineageTable.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class LineageTable extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
