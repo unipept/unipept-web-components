a715b1687b57b098b4409afa1a7c8d546c337f76
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-04-09 15:10:07 +0200

    Implement basics of LineageTree

---
+++ src/components/lineage/LineageTree.vue
@@ -0,0 +1,69 @@
+<template>
+    <v-card>
+        <v-card-text>
+            <treeview-visualization :tree="tree"></treeview-visualization>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop, Watch } from "vue-property-decorator";
+import { Peptide } from "./../../business/ontology/raw/Peptide";
+import Tree from "./../../business/ontology/taxonomic/Tree";
+import Treeview from "./../../components/visualizations/Treeview.vue";
+import TreeviewVisualization from "./../../components/visualizations/TreeviewVisualization.vue";
+import ProteinProcessor from "./../../business/processors/protein/ProteinProcessor";
+import { NcbiId } from "./../../business/ontology/taxonomic/ncbi/NcbiTaxon";
+import { CountTable } from "./../../business/counts/CountTable";
+import NcbiOntologyProcessor from "./../../business/ontology/taxonomic/ncbi/NcbiOntologyProcessor";
+
+@Component({
+    components: { TreeviewVisualization, Treeview }
+})
+export default class LineageTree extends Vue {
+    @Prop({ required: true })
+    private peptide: Peptide;
+    @Prop({ required: true })
+    private equateIl: boolean;
+
+    private tree: Tree;
+
+    private loading: boolean = false;
+
+    private mounted() {
+        this.onInputsChanged();
+    }
+
+    @Watch("peptide")
+    @Watch("equateIl")
+    private async onInputsChanged() {
+        if (this.peptide) {
+            this.loading = true;
+
+            const proteinProcessor = new ProteinProcessor();
+            const proteins = await proteinProcessor.getProteinsByPeptide(this.peptide, this.equateIl);
+
+            const taxaCounts = new Map<NcbiId, number>();
+
+            for (const protein of proteins) {
+                taxaCounts.set(protein.organism, (taxaCounts.get(protein.organism) || 0) + 1);
+            }
+
+            const taxaCountTable = new CountTable<NcbiId>(taxaCounts);
+
+            const taxaOntologyProcessor = new NcbiOntologyProcessor();
+            const taxaOntology = await taxaOntologyProcessor.getOntology(taxaCountTable);
+
+            this.tree = new Tree(taxaCountTable, taxaOntology);
+
+            this.loading = false;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
