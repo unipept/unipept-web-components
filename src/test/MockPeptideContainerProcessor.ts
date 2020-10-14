0a67e1537096954cb5e9b9aea58e604fc02cef92
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2020-10-14 10:56:10 +0200

    Update mocked worker loader

--- src/test/MockPeptideContainerProcessor.ts
+++ src/test/MockPeptideContainerProcessor.ts
@@ -1,4 +1,5 @@
-import SearchConfiguration from "@/business/configuration/SearchConfiguration";
+import { workerFunctionMap } from "@/business/system/queue/GeneralWorkerExports";
+
 
 /**
  * This is a mock implementation of the worker interface that performs all computations on the main thread. This class
@@ -7,67 +8,18 @@ import SearchConfiguration from "@/business/configuration/SearchConfiguration";
  * @author Pieter Verschaffelt
  */
 export default class Worker {
-    public async postMessage(event: { peptides: string[], config: SearchConfiguration, baseUrl: string}) {
-        let result = await process( event.peptides, event.config, event.baseUrl, (val) => this.setProgress(val) );
-        this.onmessage({
-            data: {
-                type: "result",
-                value: result
-            }
-        });
-    }
+    public async postMessage(task: any) {
+        const messageType: string = task.data.type;
+        const args: any = task.data.args;
 
-    public setProgress(value) {
+        const result = await workerFunctionMap.get(messageType)(args);
         this.onmessage({
-            data: {
-                type: "progress",
-                value: value
-            }
-        })
+            type: "result",
+            result: result
+        });
     }
 
     public onmessage(event): void {
         // Must be implemented by worker consumer by overriding this function
     }
-}
-
-const PEPTDATA_BATCH_SIZE = 100;
-const PEPTDATA_ENDPOINT = "/mpa/pept2data";
-
-async function process(peptides, config, baseUrl, setProgress) {
-    // Maps each peptide onto the response it received from the Unipept API.
-    const responses = new Map();
-
-    setProgress(0.0);
-    for (let i = 0; i < peptides.length; i += PEPTDATA_BATCH_SIZE) {
-        const data = JSON.stringify({
-            peptides: peptides.slice(i, i + PEPTDATA_BATCH_SIZE),
-            equate_il: config.equateIl,
-            missed: config.enableMissingCleavageHandling
-        });
-
-        const res = await postJSON(baseUrl + PEPTDATA_ENDPOINT, data);
-
-        res.peptides.forEach(p => {
-            responses.set(p.sequence, p);
-        })
-
-        setProgress((i + PEPTDATA_BATCH_SIZE) / peptides.length);
-    }
-
-    setProgress(1);
-    return responses;
-}
-
-async function postJSON(url, data) {
-    const result = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-        },
-        body: data,
-    });
-
-    return await result.json();
 }
