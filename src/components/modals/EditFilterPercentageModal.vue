2cadef8d0c64719d42420467b55ae2985fbf9e21
tibvdm <tibovandemoortele@hotmail.com>
2023-01-31 10:01:59 +0100

    filtering modal design + (TODO: tekst)

--- src/components/modals/EditFilterPercentageModal.vue
+++ src/components/modals/EditFilterPercentageModal.vue
@@ -2,19 +2,25 @@
     <v-dialog 
         v-model="dialogOpen" 
         max-width="500"
-        @click:outside="closeDialog"
+        @click:outside="onClickOutside"
     >
         <v-card>
             <v-card-title>
-                Filtering
+                Filter
             </v-card-title>
+
+            <v-card-subtitle>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            </v-card-subtitle>
 
             <v-card-text class="mt-2 pb-0">
                 <v-slider 
                     v-model="filterPercentage"
                     thumb-color="primary"
-                    :thumb-label="true"
-                    :thumb-size="22"
+                    thumb-label="always"
+                    thumb-size="22"
                     min="0"
                     max="100"
                 >
@@ -27,6 +33,11 @@
                     </template>
                 </v-slider>
             </v-card-text>
+
+            <v-card-actions class="pb-5 pt-0" >
+                <v-spacer></v-spacer>
+                <v-btn color="primary" @click="closeDialog">Apply filter</v-btn>
+            </v-card-actions>
         </v-card>
     </v-dialog>
 </template>
@@ -50,6 +61,11 @@ const filterPercentage = ref<number>(props.modelValue);
 const closeDialog = () => {
     dialogOpen.value = false;
     emits('update:model-value', filterPercentage.value);
+    emits('close', dialogOpen.value);
+}
+
+const onClickOutside = () => {
+    dialogOpen.value = false;
     emits('close', dialogOpen.value);
 }
 
